51e9cc72ca2fa8d7d75fe23042b4df496503a35f
M CotSIS <m1k4h1r@gmail.com>
2023-01-10 14:16:41 +0200

    Initial commit

---
+++ App.java
@@ -0,0 +1,12 @@
+
+
+
+public class App {
+
+	public static void main (String args[]){
+	
+		System.out.println("Version 0.1");
+		System.out.println(Gen.content());	
+	}
+
+}
---
+++ Gen.java
@@ -0,0 +1,8 @@
+public class Gen{
+
+
+	public static String content(){
+		return "There was a middle-aged coder who was as if he didn\'t know who to be,\n Stubbornly he wanted to fiddle about with C,\n The man saw his own reflection and removed his balaclava,\n After which he started programming in Java,\n The process took him 20 long and wasted years, it didn\'t happen all so suddenly!";
+
+	}
+}
